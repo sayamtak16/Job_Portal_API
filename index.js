d7fe80aad26e5a36d475f2f6d338051f2911a537
“sayamtak16” <sayamtak16@gmail.com>
2025-04-27 15:05:36 +0530

    JobPortal_API

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
+import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
+dotenv.config({});
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// const corsOptions = {
+//   origin: ["http://localhost:5173"],
+//   credentials: true,
+// };
+
+// app.use(cors(corsOptions));
+app.use(cors({
+  origin:true,
+  methods:[ "GET","POST","PUT","DELETE"],
+  credentials:true
+}))
+
+const PORT = process.env.PORT || 5001;
+
+app.get('/',(req,res)=> res.json({message:"This is Home Route...."}))
+
+ //Api's
+ app.use("/api/user", userRoute);
+ app.use("/api/company", companyRoute);
+ app.use("/api/job", jobRoute);
+ app.use("/api/application", applicationRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
